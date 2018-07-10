8879027817a18e4f42891603778e4cb34e612a7a
Craig Newton <newtondev@gmail.com>
2018-07-10 12:16:10 +0200

    Added bot command parsing. Added chuckJoke and yoMamaJoke fetching and sending.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const ent = require('ent');
 
 const bot = new SlackBot({
   token: process.env.BOT_TOKEN,
@@ -18,3 +19,86 @@ bot.on('start', () => {
 
 // Error Handler
 bot.on('error', () => console.log(err));
+
+
+// Message Handler
+bot.on('message', (data) => {
+  if (data.type !== 'message') {
+    return;
+  }
+
+  handleMessage(data.text);
+});
+
+// Respond to Data
+function handleMessage(message) {
+  if (message.includes(' chucknorris')) {
+    chuckJoke();
+  } else if (message.includes(' yomama')) {
+    yoMamaJoke();
+  } else if (message.includes(' random')) {
+    randomJoke();
+  } else if (message.includes(' help')) {
+    runHelp();
+  }
+}
+
+// Tell a Chuck Norris Joke
+function chuckJoke() {
+  axios.get('http://api.icndb.com/jokes/random/')
+    .then(res => {
+      const joke = ent.decode(res.data.value.joke);
+
+      const params = {
+        icon_emoji: ':laughing:'
+      };
+    
+      bot.postMessageToChannel(
+        'general', 
+        `Chuck Norris: ${joke}`, 
+        params
+      );
+    });
+}
+
+// Tell a Yo Mama Joke
+function yoMamaJoke() {
+  axios.get('http://api.yomomma.info')
+    .then(res => {
+      const joke = ent.decode(res.data.joke);
+
+      const params = {
+        icon_emoji: ':laughing:'
+      };
+    
+      bot.postMessageToChannel(
+        'general', 
+        `Yo Mama: ${joke}`, 
+        params
+      );
+    });
+}
+
+
+// Tell a Random Joke
+function randomJoke() {
+  const rand = Math.floor(Math.random() * 2) + 1;
+  if (rand === 1) {
+    chuckJoke();
+  } else if (rand === 2) {
+    yoMamaJoke();
+  }
+}
+
+// Show Help Text
+function runHelp() {
+  const params = {
+    icon_emoji: ':question:'
+  };
+
+  bot.postMessageToChannel(
+    'general', 
+    `Type @jokebot with either 'chucknorris', 'yomama' or 'random' to get a joke`, 
+    params
+  );
+}
